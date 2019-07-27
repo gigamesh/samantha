6d94af394c46afef6cf66ea4add74284e2a72946
Matthew Masurka <m.masurka@gmail.com>
2019-07-27 14:59:31 -0700

    description in WelcomeScreen

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { winLength } from "./constants";
-import "./App.scss";
+import React from 'react';
+import { winLength } from './constants';
+import './App.scss';
 
 export default function WelcomeScreen(props) {
   return (
@@ -8,16 +8,9 @@ export default function WelcomeScreen(props) {
       <div className="content">
         <h1 className="header">samantha</h1>
         <p>
-          Samantha is a multi-player memory game. Repeat the sequence of
-          sound/colors. If you can successfully remember {winLength} steps of
-          the sequence, you win!
+          Repeat the sequence of sound/colors. If you can successfully remember{' '}
+          {winLength} steps of the sequence, you win!
         </p>
-        {/* <p>
-          Samantha is a multi-player memory game. Each player takes turns adding
-          to a sequence, and the player who can remember the longest sequence is
-          the winner.
-        </p> */}
-        {/* <h1 className="header">join a game</h1> */}
         <div className="button-container">
           <button className="new-game-btn" onClick={props.closeWelcome}>
             start new game
